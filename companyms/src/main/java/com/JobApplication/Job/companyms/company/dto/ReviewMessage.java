b71606e1a834d3fa9d5800a003ff300e43232892
Bajaj <karan.bajaj@capgemini.com>
2024-01-30 09:04:08 +0530

    Job Application Microservice

---
+++ companyms/src/main/java/com/JobApplication/Job/companyms/company/dto/ReviewMessage.java
@@ -0,0 +1,14 @@
+package com.JobApplication.Job.companyms.company.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewMessage {
+    private Long id;
+    private String description;
+    private String title;
+    private double rating;
+    private Long companyId;
+}
---
+++ reviewms/src/test/java/com/JobApplication/Job/reviewms/ReviewmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobApplication.Job.reviewms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
