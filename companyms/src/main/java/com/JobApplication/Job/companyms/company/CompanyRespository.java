f6531295bb386904b8a94c9dfec88fed67106519
Karan Bajaj <bajajkaran027@gmail.com>
2024-01-30 09:27:41 +0530

    Remove folders and submodule

--- companyms/src/main/java/com/JobApplication/Job/companyms/company/CompanyRespository.java
+++
@@ -1,6 +0,0 @@
-package com.JobApplication.Job.companyms.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRespository extends JpaRepository<Company, Long> {
-}
